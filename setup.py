120be9a558926f60d4a5bb33449404078eb4b132
ilya <unicodefreak@gmail.com>
2012-11-16 02:44:01 +0400

    inc version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "akamaru",
-    version = "2.0.13",
+    version = "2.0.14",
     author = "Mikhail Turilin",
     author_email = "mturilin@gmail.com",
     description = ("Very simple social auth backend for Django "),
